9a1561f96671605fce7b2c8c43774793db87df67
Tatsuki Nagaoka <nagaoka@TatsukinoMacBook-Pro.local>
2020-05-10 17:32:01 +0900

    課題③-2 タスク状態変更機能

---
+++ js/main.js
@@ -0,0 +1,71 @@
+'use strict';
+
+{
+  const todos = [];
+
+  document.getElementById('add-button').addEventListener('click', () => {
+    const task = document.getElementById('task');
+    const todo = {
+      task: task.value,
+      status: '作業中'
+    };
+    addToDo(todo);
+    task.value = '';
+  });
+
+  // ToDo追加
+  function addToDo(todo) {
+    todos.push(todo);
+
+    const table = document.getElementById('task-table');
+    const row = table.insertRow();
+    const cellId = row.insertCell(0);
+    const cellComment = row.insertCell(1);
+    const cellStatus = row.insertCell(2);
+    const textIndex = document.createTextNode(todos.length - 1);
+    const textTask = document.createTextNode(todo.task);
+    const statusButton = createStatusButton(todo);
+    const deleteButton = createDeleteButton(todo);
+
+    cellId.appendChild(textIndex);
+    cellComment.appendChild(textTask);
+    cellStatus.appendChild(statusButton);
+    cellStatus.appendChild(deleteButton);
+  }
+
+  // ToDo削除
+  function deleteToDo(deleteButton, todo) {
+    const tr = deleteButton.parentNode.parentNode;
+    const deleteIndex = todos.indexOf(todo);
+    const tableRows = document.getElementById('task-table').rows;
+    tr.parentNode.deleteRow(tr.rowIndex);
+    todos.splice(deleteIndex, 1);
+    for (let i = 1; i < tableRows.length; i++) {
+      tableRows[i].cells[0].textContent = i - 1;
+    }
+  }
+
+  // 状態ボタン作成
+  function createStatusButton(todo) {
+    const statusButton = document.createElement('button');
+    const text = document.createTextNode(todo.status);
+    statusButton.appendChild(text);
+    statusButton.addEventListener('click' , () => {
+      const index = todos.indexOf(todo);
+      const status = (statusButton.textContent === '作業中') ? '完了' : '作業中';
+      todos[index].status = statusButton.textContent = status;
+    });
+    return statusButton;
+  }
+
+  // 削除ボタン作成
+  function createDeleteButton(todo) {
+    const deleteButton = document.createElement('button');
+    const text = document.createTextNode('削除');
+    deleteButton.appendChild(text);
+    deleteButton.addEventListener('click', function() {
+      deleteToDo(this, todo);
+    });
+    return deleteButton;
+  }
+}
